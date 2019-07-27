466799b89f05b0be867455edaf44d0a1f2bdbab6
Jithinksatheesh <jithinksatheesh@gmail.com>
2019-07-27 10:22:05 +0530

    Almost done
    Still need to change logo and backround images

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,6 +49,17 @@ $(document).ready(function() {
             $('.header').addClass('sticky_header');
         }
     });
+
+
+    /* ==============================================
+        Scroll spy
+        =============================================== */
+
+        $('.smooth-goto').on('click', function() {  
+    $('html, body').animate({scrollTop: $(this.hash).offset().top - 50}, 1000);
+    return false;
+    });
+
     /* --------------------------------------------------------
      COUNTER JS
      ----------------------------------------------------------- */
@@ -59,7 +70,7 @@ $(document).ready(function() {
     });
 
     $(".countdown")
-        .countdown("2018/03/01", function(event) {
+        .countdown("2019/09/01", function(event) {
             $(this).html(
                 event.strftime('<div>%w <span>Weeks</span></div>  <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
             );
