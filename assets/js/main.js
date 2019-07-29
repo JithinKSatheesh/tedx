8ec080abbf1bd5fdd34e0dd9a4f51892ecb35dc4
Jyothis P <jyothisp52@gmail.com>
2019-07-29 21:36:50 +0530

    Minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
         =============================================== */
 
         $('.smooth-goto').on('click', function() {  
-    $('html, body').animate({scrollTop: $(this.hash).offset().top - 50}, 1000);
+    $('html, body').animate({scrollTop: $(this.hash).offset().top - 100}, 1000);
     return false;
     });
 
